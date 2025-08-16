ae50ff019c30959322ee2cdb6902f19d733fa299
NSKV00 <nicolas.g.g.dsilva@gmail.com>
2025-08-15 21:13:41 -0300

    fix import

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,7 +2,8 @@ import { Route, Routes} from "react-router-dom"
 import { Home } from "../pages/home/Home"
 import { Agendamento } from "../pages/agendamento/Agendamento"
 import { Servicos } from "../pages/servicos/Servicos"
-import { Clientes } from "../pages/clientes/Clientes"import { Login } from "../pages/login/Login"
+import { Clientes } from "../pages/clientes/Clientes"
+import { Login } from "../pages/login/Login"
 import { Cadastro } from "../pages/cadastro/Cadastro"
 
 export function MainRoutes(){
