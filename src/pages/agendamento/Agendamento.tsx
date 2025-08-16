0116d071cddb50cedaeb9508d53db15b6d3e79fd
Bernardo <bernardorabuske67@gmail.com>
2025-08-15 21:00:28 -0300

    tela serviços base

---
+++ src/pages/agendamento/Agendamento.tsx
@@ -0,0 +1,17 @@
+import { useContext, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { Header } from "../../components/header/Header"
+import { Footer } from "../../components/footer/Footer"
+
+
+export const Agendamento = ()=>{
+    const navigate = useNavigate()
+
+    return<>
+    
+    <Header/>
+    
+    <Footer/>
+    </>
+    
+}
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { Header } from "../../components/header/Header"
 import { Footer } from "../../components/footer/Footer"
-
+import style from "./style.module.css"
 
 export const Home = ()=>{
     const navigate = useNavigate()
@@ -10,8 +10,23 @@ export const Home = ()=>{
     return<>
         <Header/>
 
-        <main>
-            <h5>ltgcltodltutgfvkyrd</h5>
+        <main className={style.main}>
+            <section className={style.Home}>
+                <div className={style.Historia}>
+                    <h1>História</h1>
+                </div>
+                <div className={style.texto}>
+                    <p>
+                        A Gravus começou pequena, na garagem de casa,
+                        com paredes simples, cadeiras de madeira e vidros antigos marcados pelo tempo.
+                        O que parecia um sonho distante ganhou forma a cada corte,
+                        a cada cliente que saía com um sorriso e a certeza de voltar.
+                        Hoje, somos uma das barbearias mais reconhecidas de Santa Cruz do Sul 
+                        — modernas no atendimento, clássicas na essência. Porque, para nós,
+                        é mais do que cortar cabelo,é sobre criar histórias e conexões que duram.
+                    </p>
+                </div>
+            </section>
         </main>
 
         <Footer/>
